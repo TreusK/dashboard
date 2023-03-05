4a7e536da12d3c2762602c47c80fb42c865bee18
TreusK <flutri@protonmail.com>
2023-03-04 21:08:44 -0300

    done

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 let overlay = document.querySelector('.overlay');
 let sidebar = document.querySelector('#dashSidebar');
 let menu = document.querySelector('.menuIcon');
+let sunIcon = document.querySelector('.sunIcon');
+let moonIcon = document.querySelector('.moonIcon');
+
 
 menu.addEventListener('click', displaySidebar);
 overlay.addEventListener('click', hideSidebar);
 window.addEventListener('resize', hideSidebarOnResize);
+sunIcon.addEventListener('click', toggleTheme);
+moonIcon.addEventListener('click', toggleTheme);
+
+function toggleTheme() {
+  const root = document.documentElement;
+  const newTheme = root.className === 'dark' ? 'light' : 'dark';
+  root.className = newTheme;
+  if(newTheme === 'dark') {
+    sunIcon.classList.remove('hide');
+    moonIcon.classList.add('hide');
+  } else {
+    moonIcon.classList.remove('hide');
+    sunIcon.classList.add('hide');
+  }
+}
 
 function displaySidebar(e) {
   overlay.classList.add("showOverlay");
